81fa02c0ef7900d551a1ccbb90ba859cc4b03a37
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2025-08-16 23:24:30 -0400

    'upgrade para deixa mais otimizado mais o select esta um caus

--- Files/player/script/inventario_coleta/removeItem.java
+++ Files/player/script/inventario_coleta/removeItem.java
@@ -1,5 +1,8 @@
 public class removeItem {
-  public void CaractItem(item rmItem, int value, SpatialObject objitem,ObjectFile drops) {
+  private int values = 1;
+
+  public void CaractItem(item rmItem, int value, SpatialObject objitem, ObjectFile drops) {
+    if (rmItem == null || value <= 0) return;
     Vector3 mypos = objitem.getGlobalPosition();
     SpatialObject drop = objitem.Instantiate(drops, mypos);
     drop.setName(rmItem.name);
@@ -7,13 +10,17 @@ public class removeItem {
     drop.getPhysics().setPhysicsEntity(new Rigidbody());
     if (drop.findComponent("item") == null) drop.addComponent(new item());
     item addDados = drop.findComponent(item.class);
+
     if (drop.findComponent("Collider") == null) drop.addComponent(new Collider());
     Collider ItemCollider = drop.findComponent("collider");
+
     if (drop.findComponent("ModelRenderer") == null) drop.addComponent(new ModelRenderer());
     ModelRenderer ItemType = drop.findComponent("ModelRenderer");
+
     if (addDados != null) {
       DadosItem(addDados, rmItem);
       addDados.QuatItemGrup = value;
+      values = 1;
     }
     if (ItemType != null) ItemType.setModelFile(rmItem.vertex);
     if (ItemCollider != null) {
@@ -22,9 +29,18 @@ public class removeItem {
     }
   }
 
+  public int valueDele() {
+    SUIText text = null;
+    if (text == null) text = WorldController.findObject("defQuatDelet").findChildObject("QuantD").findComponent("SUIText");
+    if (Input.isKeyDown("mais")) values++;
+    if (Input.isKeyDown("menos") && values > 1) values--;
+    if (text != null) text.setText("" + values);
+    return values;
+  } 
+
   private void DadosItem(item armazena, item dados) {
-      
     armazena.name = dados.name;
+    armazena.id = dados.id;
     armazena.ui = dados.ui;
     armazena.vertex = dados.vertex;
     armazena.value = dados.value;
@@ -33,5 +49,5 @@ public class removeItem {
     armazena.MapSpriteX = dados.MapSpriteX;
     armazena.MapSpriteY = dados.MapSpriteY;
     armazena.logica = dados.logica;
-  } 
+  }
 }
